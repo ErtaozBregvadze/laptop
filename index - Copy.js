424870dff5e6ec650f5908d39a1f22366705bbb9
unknown <labadze156@gmail.com>
2025-05-02 13:47:21 +0400

    gagzavna

---
+++ index - Copy.js
@@ -0,0 +1,16 @@
+
+
+const userIsAdmin = true
+const discount = userIsAdmin ? 25 : 0;
+console.log(discount);
+
+const userDistance = 45;
+const shippingCost = userDistance <= 50 ? 10 : 15;
+console.log(shippingCost);  
+
+
+const userDistance = 45;
+const shippingCost = userDistance <= 50 ? 10 : 15;
+console.log(shippingCost);  
+
+
