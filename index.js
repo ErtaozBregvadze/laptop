86eae777a47d757e787b694776654a73bb5ab6ff
Ertaoz Bregvadze <labadze156@gmail.com>
2025-05-06 18:23:23 +0400

    zvxdv

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const userDistance = 45;
 const shippingCost = userDistance <= 50 ? 10 : 15;
 console.log(shippingCost);  
 
-
+brenchrti
 let x = "10+1" 
 
 const result = eval(x)
 
 console.log(result);
 
+brnchori
